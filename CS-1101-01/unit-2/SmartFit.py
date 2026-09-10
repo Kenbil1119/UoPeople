d67552fa31df502d14326c7bdfa8eeecf745c8a8
Usman Saheed <uskehinde@gmail.com>
2026-09-10 22:41:13 +0100

    SmartFit program: add membership plan

--- CS-1101-01/unit-2/SmartFit.py
+++ CS-1101-01/unit-2/SmartFit.py
@@ -1,26 +1,39 @@
 # Ask user to enter age
 age = int(input("Please enter your age (number only!): "))
+
 # Check eligibility of age
 if age < 18:
     print("You are eligible for the Teen Fitness Program\n")
-elif age >= 18 and age <= 40:
+elif 18 <= age <= 40:
     print("You are eligible for the Regular Fitness Program\n")
 else:
     print("You are eligible for the Senior Wellness Program\n")
 
+
 # Ask about user medical condition if age is 40 or above
+medical = False
+
 if age >= 40:
-    reply = input("Do you have any medical conditions?\nReply with 'yes' or 'no': ")
-    if reply == 'yes' or reply == 'Yes':
-        print("Medical clearance required before joining.\n")
-        exit()
-    elif reply == 'no' or reply == 'No':
-        print("You can proceed with registration\n")
-    else:
-        print("Invalid input. Please enter 'yes' or 'no'.")
+    while True:
+        medical = input("Do you have any medical conditions?(yes/no): ")
+
+        if medical == 'yes' or medical == 'Yes':
+            medical = True
+            print("\nMedical clearance required before joining.\n")
+            break
+
+        elif medical == 'no' or medical == 'No':
+            medical = False
+            print("You can proceed with registration\n")
+            break
+
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+
 # Proceed if age is below 40
 else:
     print("You can proceed with registration\n")
+
 
 '''
 Ask the user to choose a membership type.
@@ -30,35 +43,55 @@ If no, print: "Basic plan: $30 per month."
 If the user selects Premium, print: "Premium plan: $60 per month."
 '''
 
-# Membership Type
 
-plan = {
-        '1': "Basic",
-        '2': "Premium"
-        }
+# Membership Plan
+Plan = {
+    '1': "Basic",
+    '2': "Premium"
+}
 
-print(f"SmartFit offers two membership types:\n1. {plan['1']}\n2. {plan['2']}")
+plan = '0'
+print(f"SmartFit offers two membership types:\n1. {Plan['1']}\n2. {Plan['2']}")
 
-loop = 1    # To iterate reply on wrong input
-while loop:
-    reply = input("Choose a Plan ('1' or '2'): ")
-    if reply == '1' or reply == '2':
+while True:
+    plan = input("Choose a Plan ('1' or '2'): ")
+    if plan == '1' or plan == '2':
         break
     else:
         print("\tInvalid respond!\nReply with: '1' or '2'\n")
-        print(f"1. {plan['1']}\n2. {plan['2']}")
+        print(f"1. {Plan['1']}\n2. {Plan['2']}")
 
-if plan[reply] == 'Basic':
-    while loop:
+# Track whether Basic uers wants personal training
+personal_training = False
+
+if Plan[plan] == 'Basic':
+    while True:
         reply = input("Do you want a personal training? (yes/no): ")
         if reply == 'yes' or reply == 'Yes':
+            personal_training = True
             print("Basic Plan with Personal Training: $45 per month.")
-            loop = 0
+            break
         elif reply == 'no' or reply == 'No':
+            personal_training = False
             print("Basic Plan: $30 per month")
-            loop = 0
+            break
         else:
             print("Reply with 'yes' or 'no'")
-            loop = 1
 else:
     print("Premium Plan: $60 per month")
+
+
+'''
+If the user has Premium membership and their age is under 30, print: "You qualify for a youth discount! 10% off your plan."
+If the user has Basic membership and does not want personal training, print: "Consider upgrading to Premium for more benefits!"
+If the user has a medical condition and chooses Premium, print: "We recommend a free consultation before starting.”
+'''
+
+if Plan[plan] == 'Premium' and age < 30:
+    print("You qualify for a youth discount! 10% off your plan.")
+
+if Plan[plan] == 'Basic' and not personal_training:
+    print("Consider upgrading to Premium for more benefits!")
+
+if Plan[plan] == 'Premium' and medical:
+    print("We recommend a free consultation before starting.")
